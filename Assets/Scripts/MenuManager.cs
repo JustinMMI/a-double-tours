2108afac8946fe455f110e77049ab387985e1213
JuJust123 <justin.esquer@mmibordeaux.com>
2026-04-02 11:21:07 +0200

    Opening Menu V1

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour
+{
+    public List<PionSelector> allPions;
+
+    public void OnStartClicked()
+    {
+        List<string> selectedPions = new List<string>();
+
+        // On vérifie qui est sélectionné
+        foreach (PionSelector p in allPions)
+        {
+            if (p.isSelected)
+            {
+                selectedPions.Add(p.pionName);
+            }
+        }
+
+        if (selectedPions.Count >= 2)
+        {
+            Debug.Log("Lancement avec " + selectedPions.Count + " joueurs.");
+
+            PlayerPrefs.SetInt("PlayerCount", selectedPions.Count);
+            for (int i = 0; i < selectedPions.Count; i++)
+            {
+                PlayerPrefs.SetString("Player_" + i, selectedPions[i]);
+            }
+
+            SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            Debug.LogWarning("Il faut au moins 2 pions sélectionnés !");
+        }
+    }
+}
